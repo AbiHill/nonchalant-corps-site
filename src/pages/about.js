9e94941aa4c9decbee769befa0160bc43dd7d43a
Abi Hill <abi.hill@bynd.com>
2020-01-31 16:45:33 +0000

    start of app

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React, { Component}  from 'react';
+
+class About extends Component {
+  constructor(props){
+    super(props);
+    this.state = {pictures : []};
+  }
+}
+
+export default About;
